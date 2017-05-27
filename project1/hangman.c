60e77443eec6e1b58754acee352cad447c1b2b08
root <root@localhost.localdomain>
2017-05-27 11:57:09 -0400

    hangman with some small revisions. the game can loop now

--- project1/hangman.c
+++ project1/hangman.c
@@ -5,116 +5,148 @@
 #include <ctype.h>
 
 char *get_word(int num_of_lines, int random_number, char word_container[], FILE *fp); 
-int word_counter(int argc, char **argv);
+int word_counter(char **argv);
 int strcmp_cap_or_lower(char user_guess, char rdom);
 
-int main(int argc, char **argv)
+int main(int arg, char **argv)
 {
 	FILE *fp;
 	fp = fopen(argv[1], "r");
 	char word_container[36];
-	//char *array = "Johnny";
-	char line[15];
-	char *word = "J";
-	char  str[20] = {'\0'};
-	//printf("The word");
-	fgets(line, 15, stdin);
-	int x = strlen(line);
-	line[x - 1] = '\0';
-	printf("length %d\n", x);
-	strcat(str, line);
-	printf("%s\n" , str);
-	if (strcasecmp (str, word) == 0) {
-		printf("the same");
-	}
 
-	int num_of_words = word_counter(argc, argv);
+	int num_of_words = word_counter(argv);
 	printf("The file count is  ----> %d", num_of_words);
-	//printf("The letter %c", line[1]);
-	//return(0);
 
-	srand(time(NULL));
-	int list_row = rand() % num_of_words;      //the row where the word will come from
-	printf("This is the random list row -----> %d \n", list_row);
+	char answer[5] = {'\0'};
+	int win = 0, loss = 0, game = 0;
 
-	printf("This is random word\n");
-	char * random = get_word(num_of_words, list_row, word_container, fp);
-	printf("This is letter in random word ---> %c\n", *(random + 1));
-	printf("This is word----> %s\n", random);
-	printf("length = %d\n", strlen(random));
-	
-	// NEW STUF***********************************************************************
-	//char user_character = getchar();
-	// visual format in these lines possibly put in it own function
-	//char display_word[37] = {'\0'};
-	char *display_word = (char*) malloc(strlen(random + 1)); //remove if doesnt work
-	int space = 1;
-	char display = '_';
-	int guess_num = 0;
-	for(int i = 0; i < strlen(random); i++){
-		printf("%*c ", space, display );
-		
-	}
-	// ******************************************* 
-	//printf("input a char: ");
-	//char mike[40];
-	//fgets(mike,40, stdin);
-	//mike[strlen(mike) - 1] = '\0';
-	//char user_guess = mike[0];
-	//int i;
-	int count = 0;
 	while(1){
-		guess_num++;
-		printf("\ninput a char: ");
-		char mike[40] =  {'\0'};
-		fgets(mike, 40, stdin);
-		mike[(strlen(mike) - 1)] = '\0';
-		char user_guess = '\0';
-		user_guess = mike[0];
-		int i;
-		for(i = 0; i < (strlen(random)); i++){
-			char rdom = '\0';
-			rdom = random[i];
-			printf("this is rdom letter ----> %c \n", rdom);
-			printf("this is guess number ---> %d and random[%d] index character ---> %c\n", guess_num, i, random[i]);
-			int comparison;
-			comparison = strcmp_cap_or_lower(user_guess, rdom);
-			printf("comparison number before the branch---> %d", comparison);
-			if(comparison == 0){
-				printf("This is print statement\n");
-				count++;
-				*(display_word + i) = user_guess; //go back to display_word[i] if it doesnt work
-			}else if(comparison != 0 && ((*(display_word + i) < 'A' && *(display_word + i) > 'Z') || (*(display_word + i) < 'a' && 
-					*(display_word + i) > 'z'))){
-				printf("This is the comparison number ----> %d\n", comparison);
-				printf("This is underscore in else\n");
-				*(display_word + i) = display;
+
+		printf("Press q to quit or any character to play\n");
+		fgets(answer, 5, stdin);
+		printf("The length of answer --> %d\n", strlen(answer));
+		//answer[strlen(answer) - 1] = '\0';
+		printf("%s", answer);
+		if(answer[0] == 'q'){
+			exit(1);
+		}
+		char answer[5] = {'\0'};
+		srand(time(NULL));
+
+	
+		int list_row = rand() % num_of_words;      //the row where the word will come from
+
+		game++;
+		
+
+
+		char * random = get_word(num_of_words, list_row, word_container, fp);
+		printf("This is word----> %s\n", random);
+		printf("length = %lu\n", strlen(random));
+
+		printf("Game %d.  %3d Win/ %3d Losses.", game, win, loss);
+		
+	
+		char *display_word = (char*) malloc(strlen(random + 1)); //remove if doesnt work
+		int space = 1;
+		char display = '_';
+		int guess_num = 0;
+		for(unsigned int i = 0; i < strlen(random); i++){
+			printf("%*c ", space, display );
+		
+		}
+		printf("\n");
+		unsigned int count = 0;
+		
+		int correct_guess = 0;
+		while(1){
+			printf("this is correct guess count %d", correct_guess);
+			if (correct_guess < 1){
+				guess_num++;
+				//correct_guess = 0;
 			}
-			if (count == (strlen(random))){
-				printf("COUNT - %d\n", count);
-				printf("LENGTH: %ld\n", strlen(random));
-				printf("Guess num is %d", guess_num);
-				printf("you won");
-				printf("random word ---->\n");
-				printf("%s", random);
+			if(guess_num == 5){
+				printf("you lost");
+				loss++;
 				break;
 			}
+			
+//			printf("\ninput a char: ");  //changing to the below line
+			
+			correct_guess = 0;
+			printf(" :");
+			
+			char mike[40] =  {'\0'};
+			fgets(mike, 40, stdin);
+			mike[(strlen(mike) - 1)] = '\0';
+			char user_guess = '\0';
+			user_guess = mike[0];
+			unsigned int i;
+			for(i = 0; i < (strlen(random)); i++){
+				char rdom = '\0';
+				rdom = random[i];
+				printf("this is rdom letter ----> %c \n", rdom);
+				printf("this is guess number ---> %d and random[%d] index character ---> %c\n", guess_num, i, random[i]);
+				int comparison;
+				comparison = strcmp_cap_or_lower(user_guess, rdom);
+				printf("comparison number before the branch---> %d", comparison);
+				if(comparison == 0){
+					if((*(display_word + i) >= 'A') && (*(display_word + i) <= 'Z')){
+						printf("Already guessed  that, guess again");
+						correct_guess++;
+						break;
+					}
+					else if((*(display_word + i) >= 'a' ) && (*(display_word + i) <= 'z')){
+						printf("Already guessed that, guess again");
+						correct_guess++;
+						break;
+					}else{
+						printf("This is print statement\n");
+						count++;
+						*(display_word + i) = user_guess; //go back to display_word[i] if it doesnt work
+						correct_guess +=1;
+						
+					}
+				}
+				else if(comparison != 0 && ((*(display_word + i) < 'A' && *(display_word + i) > 'Z') || (*(display_word + i) < 'a' && 
+						*(display_word + i) > 'z'))){
+					printf("This is the comparison number ----> %d\n", comparison);
+					printf("This is underscore in else\n");
+					*(display_word + i) = display;
+	
+					
+				}
+				if (count == (strlen(random))){
+					printf("COUNT - %d\n", count);
+					printf("LENGTH: %ld\n", strlen(random));
+					printf("Guess num is %d", guess_num);
+					printf("you won");
+					win++;
+					printf("random word ---->\n");
+					printf("%s", random);
+					break;
+				}
+			}
+		
+			printf("this is guess number---> %d", guess_num);
+			for(int x = 0; x < strlen(random); x++){
+				printf("%c", display_word[x]);
+			}
+			printf("Test to print word -----------------------> \n");
+			
+			for(int x = 0; x < strlen(random); x++){
+				printf("%c", display_word[x]);
+			}
+			if ((guess_num == 5) || (count == (strlen(random)))){
+				break;  //breaks out of game loop.
+			}
 		}
-		//display_word[i+1] = '\0';
-		printf("Test to print word -----------------------> \n");
-		for(int x = 0; x < strlen(random); x++){
- 			//printf("letter");
-			printf("%c", display_word[x]);
-		}
-		//printf("input a char: ");
-		//fgets(mike, 40, stdin);
-		//mike[(strlen(mike) - 1)] = '\0';
-		//user_guess = mike[0];
+		printf("do you want to play again?");  //big while loop to play again.
 	}
-	//****************
+	
 }
 
-int word_counter(int argc, char **argv)
+int word_counter(char **argv)
 {
 	FILE *fp;
 	char line[36];
@@ -128,9 +160,8 @@ int word_counter(int argc, char **argv)
 }
 
 
-char * get_word(int num_of_lines, int random_number, char word_container[], FILE *fp)
+char * get_word(int num_of_lines, int random_number, char *word_container, FILE *fp)
 {
-	int count = 0;
 	for (int i = 0; i <= num_of_lines; i++){
 		fgets(word_container, 36, fp);
 		if ( i == random_number){
@@ -141,7 +172,8 @@ char * get_word(int num_of_lines, int random_number, char word_container[], FILE
 }
 
 // Everything above is working so far will add everything at this function call
-int strcmp_cap_or_lower(char user_guess, char rdom)  //get user guess from  getchar in previous lines and assess for the capital or lower case and random word.
+int strcmp_cap_or_lower(char user_guess, char rdom)  //get user guess from  getchar in previous 
+//lines and assess for the capital or lower case and random word.
 {
 	int result = 0;
 	user_guess = tolower(user_guess);
